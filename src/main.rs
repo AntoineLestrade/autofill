c1da55f8e4db27953428d6753cb460ff83364984
Antoine Lestrade <antoine.lestrade@gmail.com>
2018-03-12 16:34:25 +0100

    Refactoring: Parse arguments in specific function

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,41 @@ use std::env;
 use std::fs;
 use std::path::PathBuf;
 
+struct Params {
+    program_name: String,
+    show_help: bool,
+    verbose: bool,
+    size_limit: u64,
+    source_folders: Vec<String>,
+
+    dest_folder: std::path::PathBuf
+}
+
+fn parse_arguments() -> Result<(Params, Options), getopts::Fail> {
+    let args: Vec<String> = env::args().collect();
+
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "print this help menu");
+    opts.optflag("v", "verbose", "print more output");
+    let matches = opts.parse(&args[1..])?;
+
+    let size_limit = match matches.free[0].parse() {
+        Ok(v) => v,
+        Err(_) => return Err(getopts::Fail::UnexpectedArgument(format!("Cannot parse size {} into number", matches.free[0])))
+    };
+
+    Ok((Params {
+        program_name: args[0].clone(),
+        show_help: matches.opt_present("h"),
+        verbose: matches.opt_present("v"),
+        size_limit,
+
+        source_folders: matches.free[1..matches.free.len() - 1].to_vec(),
+        dest_folder: std::path::PathBuf::from(matches.free[matches.free.len() - 1].clone())
+    }, opts))
+
+}
+
 fn list_files(
     path: &std::path::PathBuf,
     mut files_list: &mut Vec<PathBuf>) {
@@ -29,45 +64,36 @@ fn print_usage(program: &str, opts: &Options) {
     print!("{}", opts.usage(&brief));
 }
 pub fn main() {
-    let args: Vec<String> = env::args().collect();
-    let program = args[0].clone();
-
-    let mut opts = Options::new();
-    opts.optflag("h", "help", "print this help menu");
-    opts.optflag("v", "verbose", "print more output");
-    //opts.optmulti("f", "filter", "Regular expression that filters inculded files", "REGEX");
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => { m }
-        Err(f) => { panic!(f.to_string()) }
-    };
-    if matches.opt_present("h") || matches.free.len() < 3 {
-        print_usage(&program, &opts);
-        return;
-    }
-    let size_limit: u64 = match matches.free[0].parse() {
-        Ok(u) => { u }
-        Err(f) => { panic!(f.to_string()) }
-    };
-
-
-    let verbose: bool = matches.opt_present("v");
-    let target = std::path::PathBuf::from(&matches.free[&matches.free.len()-1]);
-    let mut files_list = Vec::<PathBuf>::new();
-    for index in 1..&matches.free.len() - 1 {
-        if verbose {
-            println!("Listing files of folder {}.", &matches.free[index]);
+    let params = match parse_arguments() {
+        Ok((p, opts)) => {
+            if p.show_help {
+                print_usage(&p.program_name,&opts);
+                return;
+            }
+            p
+        },
+        Err(e) => {
+            panic!("Error: {}.
+Please refer to the help for correct usage.", e);
         }
-        list_files(&std::path::PathBuf::from(&matches.free[index]), &mut files_list);
+    };
+
+    let mut files_list = Vec::<PathBuf>::new();
+    for p in params.source_folders {
+        if params.verbose {
+            println!("Listing files of folder {}.", p);
+        }
+        list_files(&PathBuf::from(p), &mut files_list);
     }
-    if verbose {
+    if params.verbose {
         println!("Files selected as potential candidates: {}", files_list.len());
     }
 
-    let mut remain = size_limit;
+    let mut remain = params.size_limit;
 
     let mut generator = rand::thread_rng();
 
-    while files_list.len() > 0 {
+    while !files_list.is_empty() {
         let index = generator.gen_range(0, files_list.len() - 1);
         let path: PathBuf = files_list.remove(index);
 
@@ -80,7 +106,7 @@ pub fn main() {
                 return;
             }
             else if let Some(file_name) = path.file_name() {
-                let target_file = target.join(std::path::Path::new(file_name));
+                let target_file = params.dest_folder.join(std::path::Path::new(file_name));
                 if fs::metadata(&target_file).is_err() {
                     // if not error, that means the target file already exists;
 
